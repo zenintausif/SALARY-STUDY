bcc8e8c3cdf872bc6fdd2373833d496cca212929
zenintausif <tausif.faiyaz14@gmail.com>
2025-01-01 21:03:57 +0000

    Cleaning data

--- salaryanalysis.py
+++ salaryanalysis.py
@@ -5,3 +5,13 @@ import seaborn as sns
 
 data = pd.read_csv("Employe_Performance_dataset.csv")
 print(data.head())
+
+print("Initial Missing Values:\n")
+print(data.isnull().sum())
+
+#Drop rows with missing values
+data.dropna(subset=['Performance Score', 'Joining Date'], inplace=True)
+
+#Re-check for Missing Values Post-Cleaning
+print("\nMissing Values After Cleaning:\n")
+print(data.isnull().sum())
